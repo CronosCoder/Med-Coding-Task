d4bf850e125ed02ea45543d91635f28091828d13
Foysal Ahmed <mfoysal314@gmail.com>
2024-04-30 15:23:47 +0600

    product showing done

--- src/product/admin.py
+++ src/product/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [ 'id','title','sku' ]
+
+
+@admin.register(ProductVariant)
+class ProductVariantAdmin(admin.ModelAdmin):
+    list_display = [ 'id','variant_title','variant','product' ]
+
+
+@admin.register(ProductVariantPrice)
+class ProductVariantPriceAdmin(admin.ModelAdmin):
+    list_display = [ 'id','product','price','stock' ]
--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,6 +1,5 @@
 from django.views import generic
-
-from product.models import Variant
+from product.models import Variant,Product,ProductVariantPrice
 
 
 class CreateProductView(generic.TemplateView):
@@ -11,4 +10,32 @@ class CreateProductView(generic.TemplateView):
         variants = Variant.objects.filter(active=True).values('id', 'title')
         context['product'] = True
         context['variants'] = list(variants.all())
+        return context
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'products/list.html'
+    context_object_name = 'products'
+    ordering = ['id']
+    paginate_by = 5  # Number of items per page
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product_data = []
+        for product in context['products']:
+            variants = ProductVariantPrice.objects.filter(product=product)
+            variant_data = []
+            for variant in variants:
+                variant_data.append({
+                    'title': f"{variant.product_variant_one} | {variant.product_variant_two} | {variant.product_variant_three}",
+                    'price': variant.price,
+                    'stock': variant.stock
+                })
+            product_data.append({
+                'id' : product.id,
+                'title': product.title,
+                'description': product.description,
+                'variants': variant_data
+            })
+        context['products'] = product_data
         return context
