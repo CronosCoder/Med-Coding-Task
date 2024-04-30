c7a3ab9b78f0e77ca9d4697244181ab571950127
Foysal Ahmed <mfoysal314@gmail.com>
2024-05-01 03:33:45 +0600

    search filter added

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,5 +1,5 @@
 from django.views import generic
-from django.core.paginator import Paginator
+from django.db.models import Q
 from product.models import Variant,Product,ProductVariantPrice,ProductVariant
 
 
@@ -19,6 +19,28 @@ class ProductListView(generic.ListView):
     context_object_name = 'products'
     ordering = ['id']
     paginate_by = 2 
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        title = self.request.GET.get('title')
+        variant = self.request.GET.get('variant')
+        price_from = self.request.GET.get('price_from')
+        price_to = self.request.GET.get('price_to')
+        date = self.request.GET.get('date')
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        if variant:
+            queryset = queryset.filter(Q(productvariant__id=variant) |
+                                       Q(productvariant__id=variant))
+        if price_from:
+            queryset = queryset.filter(productvariantprice__price__gte=price_from)
+        if price_to:
+            queryset = queryset.filter(productvariantprice__price__lte=price_to)
+        if date:
+            queryset = queryset.filter(created_at__date=date)
+
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
