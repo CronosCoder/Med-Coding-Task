d21cfb350a74a51769e545574198f23047dea1ab
Foysal Ahmed <mfoysal314@gmail.com>
2024-04-30 16:10:44 +0600

    pagination added

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -18,7 +18,7 @@ class ProductListView(generic.ListView):
     template_name = 'products/list.html'
     context_object_name = 'products'
     ordering = ['id']
-    paginate_by = 5  # Number of items per page
+    paginate_by = 2  # Number of items per page
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -31,7 +31,7 @@ class ProductListView(generic.ListView):
         products = context['products'][start_index - 1:end_index]
 
         product_data = []
-        for product in products:
+        for product in context['products']:
             variants = ProductVariantPrice.objects.filter(product=product)
             variant_data = []
             for variant in variants:
@@ -49,5 +49,5 @@ class ProductListView(generic.ListView):
         context['products'] = product_data
         context['start_index'] = start_index
         context['end_index'] = end_index
-        
+
         return context
